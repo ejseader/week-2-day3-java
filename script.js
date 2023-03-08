90921a33bbcdec4181d26accfe42f3b5958d03fc
Eric Seader <45432901+ejseader@users.noreply.github.com>
2023-03-08 11:05:35 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var color = 'orange '; /*string*/
+        var num = 10; /*number*/
+        var myStudentsAreAwesome = true; /*boolean*/
+        var thisClassIsEasy = false;
+
+// typeof + value = will tell you if it's a number, string, etc.
+
+        console.log(5 / 5);
